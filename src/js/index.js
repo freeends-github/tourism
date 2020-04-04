7b684900d66743eef22a391ae9d0901642a30797
Ragip Gjinovci <ragip.gjinovci@probit-kos.com>
2020-04-04 20:56:17 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.hotel-carousel').slick();
+});
+
+// $(".search_div button").on("click", function() {
+//   $(".input_div input").toggleClass("showSearch");
+//   if($(".input_div input").hasClass("showSearch")) {
+//     $(".second_header_second_div").css("justify-content", "flex-end");
+//     $(".list_div").css("display", "none");
+//   }
+//   else {
+//     setTimeout(function() {
+//       $(".second_header_second_div").css("justify-content", "space-between");
+//       $(".list_div").css("display", "block");
+//     }, 300);
+  
+//   }
+// });
