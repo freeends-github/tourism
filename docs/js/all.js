1bd26009834cdf2c48fc903d392a0c4f4907f661
Fatbardh <fatbardhemini5@gmail.com>
2020-04-11 20:29:13 -0700

    Version 3.0 responsive layout

--- docs/js/all.js
+++ docs/js/all.js
@@ -27,3 +27,29 @@ search.addEventListener("click",function(){
 	search.classList.toggle("rotate");
 	input.value = "";
 });
+
+var hamburger = document.querySelector('.hamburger_button');
+var pop = document.querySelector('.pop_list_container');
+var body = document.querySelector('body');
+var lines = document.querySelectorAll('.hamburger div')
+
+hamburger.addEventListener("click",function(){
+	pop.classList.toggle("open2");
+	body.classList.toggle("scroll");
+	lines.forEach(function(line){
+		line.classList.toggle("change");
+	})
+});
+
+var imgs = document.querySelectorAll('.places_img');
+// var info = document.querySelector('.places_info-2');
+// var button = document.querySelector('.places_img button');
+
+imgs.forEach(function(img){
+	var info = img.children[1];
+	var button = img.children[2];
+	button.addEventListener("click",function(){
+		info.classList.toggle("appair");
+		button.classList.toggle("spin");
+	});
+});
